1e5f31f3bfb09227570cf10f607caca8c2c89515
Tobias Wood <tobias@spinicist.org.uk>
2021-11-25 23:19:14 +0000

    Add SDC plot

--- riesling/plots.py
+++ riesling/plots.py
@@ -7,24 +7,27 @@ import colorcet as cc
 import warnings
 plt.rcParams['font.size'] = 20
 
+
 def basis(path):
     f = h5py.File(path, 'r')
     basis = f['basis'][:]
     f.close()
-    fig, ax = plt.subplots(figsize=(16,6))
+    fig, ax = plt.subplots(figsize=(16, 6))
     ax.plot(basis.T)
     ax.legend([str(x) for x in range(basis.shape[1])])
     plt.show()
+
 
 def dynamics(path):
     f = h5py.File(path, 'r')
     dyn = f['dynamics'][:]
     f.close()
-    fig, ax = plt.subplots(figsize=(16,6))
+    fig, ax = plt.subplots(figsize=(16, 6))
     ax.plot(dyn)
     plt.show()
 
-def traj2d(filename, sl_read=slice(0,-1,1), sl_spoke=slice(0,-1,1)):
+
+def traj2d(filename, sl_read=slice(0, -1, 1), sl_spoke=slice(0, -1, 1)):
     with h5py.File(filename) as f:
         traj = np.array(f['trajectory'])
         fig, ax = plt.subplots(1, 3, figsize=(12, 4))
@@ -44,21 +47,23 @@ def traj2d(filename, sl_read=slice(0,-1,1), sl_spoke=slice(0,-1,1)):
         plt.close()
     return fig
 
-def traj3d(filename, sl_read=slice(0,-1,1), sl_spoke=slice(0,-1,1)):
+
+def traj3d(filename, sl_read=slice(0, -1, 1), sl_spoke=slice(0, -1, 1)):
     with h5py.File(filename) as ff:
         traj = np.array(ff['trajectory'])
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         ax.scatter(traj[sl_spoke, sl_read, 0],
-                    traj[sl_spoke, sl_read, 1],
-                    traj[sl_spoke, sl_read, 2])
+                   traj[sl_spoke, sl_read, 1],
+                   traj[sl_spoke, sl_read, 2])
         fig.tight_layout()
         plt.close()
     return fig
 
-def kspace(filename, dset='noncartesian', vol=0, coil=0, sl_read=slice(0,-1,1), sl_spoke=slice(0,512,1)):
+
+def kspace(filename, dset='noncartesian', vol=0, coil=0, sl_read=slice(0, -1, 1), sl_spoke=slice(0, 512, 1)):
     with h5py.File(filename) as f:
-        data = np.array(f['noncartesian'][vol,sl_spoke,sl_read,coil]).squeeze().T
+        data = np.array(f[dset][vol, sl_spoke, sl_read, coil]).squeeze().T
         fig, ax = plt.subplots(2, 1, figsize=(12, 6))
         ax[0].imshow(np.log(np.abs(data+1E-10)))
         ax[1].imshow(np.angle(data))
@@ -67,6 +72,22 @@ def kspace(filename, dset='noncartesian', vol=0, coil=0, sl_read=slice(0,-1,1), 
         ax[1].set_ylabel('Readout')
         ax[0].axis('auto')
         ax[1].axis('auto')
+        fig.tight_layout()
+        plt.close()
+    return fig
+
+
+def sdc(filename, dset='sdc', sl_read=slice(0, -1, 1), sl_spoke=slice(0, 512, 1)):
+    with h5py.File(filename) as f:
+        data = np.array(f[dset][sl_spoke, sl_read]).T
+        print(data[:, 0])
+        fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+        im = ax.imshow(data, interpolation='nearest')
+        ax.set_xlabel('Spoke')
+        ax.set_ylabel('Readout')
+        ax.axis('auto')
+        ax.axis('auto')
+        fig.colorbar(im, location='right')
         fig.tight_layout()
         plt.close()
     return fig
