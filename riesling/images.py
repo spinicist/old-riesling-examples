9a0cf97d9b8edcb905dc7240e876595c18d47a96
Tobias Wood <tobias@spinicist.org.uk>
2022-02-03 20:57:29 +0000

    Big rewrite

--- riesling/images.py
+++ riesling/images.py
@@ -25,7 +25,7 @@ def get_comp(component):
     return fn
 
 
-def single(file, dset='image', title=None, pos=None, iv=0, ie=0, comp='mag', cmap='gray', clim=None):
+def planes(file, dset='image', title=None, pos=None, iv=0, ie=0, comp='mag', cmap='gray', clim=None):
     """3 plane plot of 3D image data in an h5 file
 
     Args:
@@ -60,14 +60,14 @@ def single(file, dset='image', title=None, pos=None, iv=0, ie=0, comp='mag', cma
 
     fig, ax = plt.subplots(1, 3, figsize=(
         16, 6), facecolor='black')
-    ax[0].imshow(np.squeeze(img[pos[2], :, :]),
-                 cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    ax[0].imshow(np.squeeze(img[pos[2], :, :]).T,
+                 cmap=cmap, vmin=clim[0], vmax=clim[1])
     ax[0].axis('image')
-    ax[1].imshow(np.squeeze(img[:, pos[1], :]),
-                 cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    ax[1].imshow(np.squeeze(img[:, pos[1], :]).T,
+                 cmap=cmap, vmin=clim[0], vmax=clim[1])
     ax[1].axis('image')
-    im = ax[2].imshow(np.squeeze(img[:, :, pos[0]]),
-                      cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    im = ax[2].imshow(np.squeeze(img[:, :, pos[0]]).T,
+                      cmap=cmap, vmin=clim[0], vmax=clim[1])
     ax[2].axis('image')
     cb = fig.colorbar(im, location='right', ax=ax[2])
     cb.ax.yaxis.set_tick_params(color='w', labelcolor='w')
@@ -77,92 +77,154 @@ def single(file, dset='image', title=None, pos=None, iv=0, ie=0, comp='mag', cma
     return fig
 
 
-def multi(file, dset='basis-images', title=None, pos=None, iv=0, comp='mag', cmap=None, clim=None):
-    """3 plane plot of 3D basis image data in an h5 file
-
-    Args:
-    """
-
-    f = h5py.File(file, 'r')
-    I = f[dset][:]
-
-    [nv, nz, ny, nx, ne] = np.shape(I)
-    if not (pos):
-        pos = (int(nx/2), int(ny/2), int(nz/2))
-
-    fn = get_comp(comp)
-    img = fn(np.squeeze(I[iv, :, :, :, :]))
-    if not clim:
-        clim = np.nanpercentile(img, (2, 98))
-        if clim[0] < 0:
-            clim[0] = -np.maximum(np.absolute(clim[0]), clim[1])
-            if not cmap:
-                cmap = 'cet_bkr'
-    if not cmap:
-        cmap = 'gray'
-
-    fig, ax = plt.subplots(ne, 3, figsize=(16, 6*ne), facecolor='black')
-    for ie in range(ne):
-        ax[ie, 0].imshow(np.squeeze(img[pos[2], :, :, ie]),
-                         cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-        ax[ie, 0].axis('off')
-        ax[ie, 1].imshow(np.squeeze(img[:, pos[1], :, ie]),
-                         cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-        ax[ie, 1].axis('off')
-        im = ax[ie, 2].imshow(np.squeeze(img[:, :, pos[0], ie]),
-                              cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-        ax[ie, 2].axis('off')
-        cb = fig.colorbar(im, location='right', ax=ax[ie, 2])
-        cb.ax.yaxis.set_tick_params(color='w', labelcolor='w')
-    fig.suptitle(title, color='white')
-    fig.tight_layout(pad=0)
-    plt.close()
-    return fig
-
-
-def slices(file, dset='image', title=None, nrows=1, iz=None, ie=0, comp='mag', cmap='gray', clim=None):
-    """Plot a slice through each volume of a dataset
+def slices(file, dset='image', title=None, ie=0, iv=0,
+           nslice=4, nrows=1, axis='z', start=None, stop=None,
+           comp='mag', cmap='gray', clim=None):
+    """Plot slices along one axis
 
     Args:
         file (str): .h5 file to load
         dset (str): Dataset within the .h5 file (default 'image')
         title (str): Plot title. Defaults to ''
+        nslice (int): Number of slices
         nrows (int): Number of rows to plot slices over
-        iv   (int): Volume to slice, default 0
+        axis (str) : 'x' 'y' or 'z'
         ie   (int): Echo index, default 0
-        cmap (str): colormap. Defaults to 'gray'.
-        vmin (float): Lower window limit. Defaults to None.
-        vmax (float): Upper window limit. Defaults to None.
+        iv   (int): Volume to slice, default 0
         comp (str, opt): mag/pha/real/imaginary. Default mag
+        cmap (str): colormap. Defaults to 'gray'.
+        clim (float, float): Lower/upper window limits
     """
 
     f = h5py.File(file, 'r')
     I = f[dset][:]
-
-    [nv, nz, ny, nx, ne] = np.shape(I)
-    if not iz:
-        iz = int(nz/2)
-
-    ncols = int(np.ceil(nv / nrows))
-
     fn = get_comp(comp)
-    img = fn(np.squeeze(I[:, iz, :, :, ie]))
+    img = fn(I[iv, :, :, :, ie])
     if not clim:
         clim = np.nanpercentile(img, (2, 98))
+    [nz, ny, nx] = np.shape(img)
 
-    fig, ax = plt.subplots(nrows, ncols, figsize=(
+    if axis == 'z':
+        maxn = nz
+    elif axis == 'y':
+        maxn = ny
+    else:
+        maxn = nx
+    if not start:
+        start = maxn / 4
+    if not stop:
+        end = 3 * maxn / 4
+    slpos = np.floor(np.linspace(start, stop, nslice, endpoint=True))
+
+    ncols = int(np.ceil(nslice / nrows))
+    fig, all_ax = plt.subplots(nrows, ncols, figsize=(
         3*ncols, 3*nrows), facecolor='black')
 
-    for iv in range(nv):
-        data = np.squeeze(img[iv, :, :])
-        if nrows == 1:
-            this_ax = ax[iv]
+    for ir in range(nrows):
+        if nrows > 1:
+            sl_ax = ax[ir, :]
         else:
-            this_ax = ax[int(np.floor(iv / ncols)), iv % ncols]
-        this_ax.imshow(data, cmap=cmap,
-                       vmin=clim[0], vmax=clim[1], origin='lower')
-        this_ax.axis('off')
+            sl_ax = all_ax
+        for ic in range(ncols):
+            sl = (ir * ncols) + ic
+            if sl < nslice:
+                ax = sl_ax[ic]
+                if axis == 'z':
+                    data = np.squeeze(img[slpos[sl], :, :])
+                elif axis == 'y':
+                    data = np.squeeze(img[:, slpos[sl], :])
+                else:
+                    data = np.squeeze(img[:, :, slpos[sl]])
+                im = ax.imshow(data, cmap=cmap,
+                               vmin=clim[0], vmax=clim[1], origin='lower')
+                ax.axis('off')
+    cb = fig.colorbar(im, location='right')
+    cb.ax.yaxis.set_tick_params(color='w', labelcolor='w')
     fig.tight_layout(pad=0)
+    fig.suptitle(title, color='white')
+    plt.close()
+    return fig
+
+
+def series(file, dset='image', title=None, which='echoes', io=0,
+           nrows=1, axis='z', slpos=None,
+           comp='mag', cmap='gray', clim=None):
+    """One slice through echoes or volumes
+
+    Args:
+        file (str): .h5 file to load
+        dset (str): Dataset within the .h5 file (default 'image')
+        title (str): Plot title. Defaults to ''
+        which (str): 'echoes' or 'vols'
+        io (int): Index into 'other' dim (echoes/vols)
+        nrows (int): Number of rows to plot slices over
+        axis (str) : 'x' 'y' or 'z'
+        slpos (int): Which slice
+        comp (str, opt): mag/pha/real/imaginary. Default mag
+        cmap (str): colormap. Defaults to 'gray'.
+        clim (float, float): Lower/upper window limits
+    """
+
+    f = h5py.File(file, 'r')
+    I = f[dset][:]
+    [nv, nz, ny, nx, ne] = np.shape(I)
+    fn = get_comp(comp)
+    if which == 'echoes':
+        nslice = ne
+        if axis == 'z':
+            if not slpos:
+                slpos = int(np.floor(nz / 2))
+            img = fn(I[io, slpos, :, :, :]).transpose((2, 0, 1))
+        if axis == 'y':
+            if not slpos:
+                slpos = int(np.floor(ny / 2))
+            img = fn(I[io, :, slpos, :, :]).transpose((2, 0, 1))
+        if axis == 'x':
+            if not slpos:
+                slpos = int(np.floor(nx / 2))
+            img = fn(I[io, :, :, slpos, :]).transpose((2, 0, 1))
+    else:
+        nslice = nv
+        if axis == 'z':
+            if not slpos:
+                slpos = nz / 2
+            img = fn(I[:, slpos, :, :, io])
+        if axis == 'y':
+            if not slpos:
+                slpos = ny / 2
+            img = fn(I[:, :, slpos, :, io])
+        if axis == 'x':
+            if not slpos:
+                slpos = nx / 2
+            img = fn(I[:, :, :, slpos, io])
+
+    if not clim:
+        clim = np.nanpercentile(img, (2, 98))
+        if clim[0] < 0:
+            clim[1] = np.max(np.abs(clim))
+            clim[0] = -clim[1]
+
+    ncols = int(np.ceil(nslice / nrows))
+    fig, all_ax = plt.subplots(nrows, ncols, figsize=(
+        3*ncols, 3*nrows), facecolor='black')
+
+    for ir in range(nrows):
+        if nrows > 1:
+            sl_ax = all_ax[ir, :]
+        else:
+            sl_ax = all_ax
+        for ic in range(ncols):
+            sl = (ir * ncols) + ic
+            if sl < nslice:
+                ax = sl_ax[ic]
+                data = np.squeeze(img[sl, :, :])
+                im = ax.imshow(data.T, cmap=cmap,
+                               vmin=clim[0], vmax=clim[1])
+                ax.axis('off')
+    fig.tight_layout(pad=0)
+    cb = fig.colorbar(im, location='right', ax=all_ax)
+    cb.ax.yaxis.set_tick_params(color='w', labelcolor='w')
+    cb.ax.xaxis.set_tick_params(color='w', labelcolor='w')
     fig.suptitle(title, color='white')
     plt.close()
     return fig
@@ -318,20 +380,21 @@ def grid(file, dset='cartesian', title=None, pos=None, ic=0, ie=0, comp='log', c
     if not clim:
         # There are a lot of zeros in typical cartesian grids. Use an expanded range
         clim = np.nanpercentile(img, (0, 100))
-    fig, ax = plt.subplots(1, 3, figsize=(
-        16, 6), facecolor='black', constrained_layout=True)
-    ax[0].imshow(np.squeeze(img[pos[2], :, :]),
-                 cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-    ax[0].axis('image')
-    ax[1].imshow(np.squeeze(img[:, pos[1], :]),
-                 cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-    ax[1].axis('image')
-    im = ax[2].imshow(np.squeeze(img[:, :, pos[0]]),
-                      cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
-    ax[2].axis('image')
-    cb = fig.colorbar(im, ax=ax, location='right')
+    fig, ax = plt.subplots(2, 2, figsize=(8, 8), facecolor='black')
+    ax[0, 0].imshow(np.squeeze(img[pos[2], :, :]),
+                    cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    ax[0, 0].axis('image')
+    ax[0, 1].imshow(np.squeeze(img[:, pos[1], :]),
+                    cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    ax[0, 1].axis('image')
+    im = ax[1, 0].imshow(np.squeeze(img[:, :, pos[0]]),
+                         cmap=cmap, vmin=clim[0], vmax=clim[1], origin='lower')
+    ax[1, 0].axis('image')
+    ax[1, 1].set_facecolor('k')
+    cb = fig.colorbar(im, ax=ax[1, 1], location='right')
     cb.ax.xaxis.set_tick_params(color='w', labelcolor='w')
     cb.ax.yaxis.set_tick_params(color='w', labelcolor='w')
+    fig.tight_layout(pad=0)
     fig.suptitle(title, color='white')
     plt.close()
     return fig
